3d8bbe50873d98120865ea99d2d2207c35a0587c
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2022-05-09 01:09:02 -0700

    Templates Added ✅

--- base/views.py
+++ base/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('Home Page')
+    return render(request, 'home.html')
 
 # Another example route for practice
 def room(request):
-    return HttpResponse('Rooms')
+    return render(request, 'room.html')
