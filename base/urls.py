db8fc912c258be5ae02aa9942e5e45cbc23aced6
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2022-05-10 00:13:27 -0700

    Worked with Templates and Dynamic Urls

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,5 @@ from . import views
 # Specify paths 
 urlpatterns = [
     path('', views.home , name= 'home' ),
-    path('room/' , views.room , name= 'room'),
+    path('room/<str:pk>/' , views.room , name= 'room'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Time to pass data to the templates 
+rooms = [
+    {'id': 1, 'name': 'Lets learn python with me!'},
+    {'id': 2, 'name': 'Django for begginners!'},
+    {'id': 3, 'name': 'Frontend Developers!'}
+
+] # Now we need to pass this data via home method below
+
+bugs = [
+    [1],
+    [2],
+    [3]
+]
+
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms, 'bugs': bugs} 
+    return render(request, 'base/home.html', context)
 
 # Another example route for practice
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk): #lets make urls dynamic
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+
+    context = {'room': room}
+
+    return render(request, 'base/room.html', context)
